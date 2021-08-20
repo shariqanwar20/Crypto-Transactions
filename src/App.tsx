e1b204754e88e26365c8e25cee06c35d3c966f97
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-08-21 00:35:39 +0500

    you can now send erc-20/ether to any address && your latest balance also gets displayed

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Web3 from "web3";
-import { getEthBalance } from "./Web3";
+import { getAccountBalance, sendTransaction } from "./Web3";
+import { tokenAddresses } from "./constants";
+import { Formik } from 'formik';
+// const dotenv = require("dotenv")
+// const { REACT_APP_PRIVATE_KEY_WALLET } = process.env
+
 declare let window: any;
 
 function App() {
   const [web3, setWeb3] = useState<Web3>();
   const [balance, setBalance] = useState("");
+  const [walltetAddresses, setWalletAddresses] = useState([""])
 
   useEffect(() => {
     const connectToMetamask = async () => {
@@ -18,10 +24,11 @@ function App() {
         setWeb3(web3);
 
         /* if user is already logged in get his balance */
-        web3.eth.getAccounts().then(async (accounts) => {
-          if (accounts.length > 0) {
+        web3.eth.getAccounts().then(async (_walltetAddresses) => {
+          if (_walltetAddresses.length > 0) {
             /* get user's balance */
-            setBalance(await getEthBalance(web3));
+            setBalance(await getAccountBalance(web3, _walltetAddresses[0]));
+            setWalletAddresses(_walltetAddresses)
           }
         })
       } else {
@@ -35,12 +42,18 @@ function App() {
     try {
       await window.ethereum.enable();
       /* get user's balance */
-      setBalance(await getEthBalance(web3!));
+      setBalance(await getAccountBalance(web3!, walltetAddresses[0]));
       return true;
     } catch (error) {
       return false;
     }
   };
+
+  const handleSubmit = async(values: any) => {
+    console.log("transaction pending => ", values);
+    const transaction = await sendTransaction(web3!, walltetAddresses[0], values.toAddress, values.amountToSend)
+    setBalance(await getAccountBalance(web3!, walltetAddresses[0]))
+  }
 
   return (
     <div className="App">
@@ -57,6 +70,27 @@ function App() {
         >
           {balance}
         </a>
+        <Formik
+      initialValues={{
+        toAddress: "",
+        amountToSend: 0
+      }}
+      // validationSchema={validationSchema}
+      onSubmit={(values: any) => {
+        handleSubmit(values)
+      }}
+    >
+      {({ values, touched, errors, handleSubmit, handleChange }) => (
+        <form onSubmit={handleSubmit}>
+                  <input type="text" onChange={handleChange} value={values.toAddress} name="toAddress" placeholder="Enter address to send ether/dai" />
+                  <input type="number" onChange={handleChange} value={values.amountToSend} name="amountToSend" placeholder="number of tokens to send" />
+                  <button type="submit">
+                    Send
+                  </button>
+                  </form>
+      )}
+      </Formik>
+
       </header>
       <button onClick={handleLogin}>Connect with metamask</button>
     </div>
--- src/Web3/index.ts
+++ src/Web3/index.ts
@@ -1,9 +1,57 @@
 import Web3 from "web3";
+import { tokenABI } from "../constants";
 
-export const getEthBalance = async (web3: Web3) => {
-    const accounts = await web3.eth.getAccounts();
-    let balance = await web3.eth.getBalance(accounts[0]);
-    return web3.utils.fromWei(balance, "ether");
-}
+/* get ether token balancein your wallet */
+// export const getEthBalance = async (web3: Web3, walletAddress: string) => {
+//   let balance = await web3.eth.getBalance(walletAddress);
+//   return web3.utils.fromWei(balance, "ether");
+// };
 
+/* get any ERC-20 token balance in your wallet */
+export const getAccountBalance = async (
+  web3: Web3,
+  walletAddress: string,
+  contractAddress?: string
+) => {
+    if (contractAddress === undefined) {
+        let balance = await web3.eth.getBalance(walletAddress);
+        return web3.utils.fromWei(balance, "ether");
+    }
+    else {
+        const contract = new web3.eth.Contract(tokenABI, contractAddress);
+        const balance = await contract.methods.balanceOf(walletAddress).call();
+        return web3.utils.fromWei(balance.toString());
+    }
 
+};
+
+export const sendTransaction = async (
+  web3: Web3,
+  walletAddress: string,
+  toAddress: string,
+  amountToSend: number,
+  contractAddress?: string
+) => {
+  let trans: any, contract: any;
+  if (contractAddress !== undefined) {
+    contract = new web3.eth.Contract(tokenABI, contractAddress);
+  }
+
+  let currentBalance: string = await getAccountBalance(web3, walletAddress, contractAddress)
+
+  if (Number(currentBalance) > amountToSend) {
+    contractAddress !== undefined
+      ? (trans = await contract.methods
+          .transfer(toAddress, web3.utils.toWei(amountToSend.toString()))
+          .send({ from: walletAddress }))
+      : (trans = await web3.eth.sendTransaction({
+          to: toAddress,
+          from: walletAddress,
+          value: web3.utils.toWei(amountToSend.toString()),
+        }));
+    console.log("amount send => ", trans);
+    return trans;
+  } else {
+    console.log("not enough balance");
+  }
+};
